464f945c96020eb16db7bd24dfbee1a9dd558d45
Jay kim <90327424+jaykim-github@users.noreply.github.com>
2022-07-28 23:18:45 +0900

    Time: 1 ms (98.50%), Space: 43.1 MB (43.08%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> list = new ArrayList<>();
+        int max = 0;
+        
+        for(int i = 0; i<candies.length; i++){
+            if(max<candies[i])
+                max = candies[i];
+        }
+        
+        for(int i = 0; i<candies.length; i++){
+            if(candies[i] + extraCandies < max){
+                list.add(false);
+            }else{
+                list.add(true);
+            }
+        }
+        
+        return list;
+    }
+}
