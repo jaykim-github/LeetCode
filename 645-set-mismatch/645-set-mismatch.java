14e6b53afb70883dda6bec51d1efc394cc5baced
Jay kim <90327424+jaykim-github@users.noreply.github.com>
2022-09-28 10:52:59 +0900

    Time: 746 ms (5.06%), Space: 54.4 MB (69.49%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.java
+++ 645-set-mismatch/645-set-mismatch.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int[] findErrorNums(int[] nums) {
-        int dup = -1, missing = -1;
+        int dup = -1, missing = -1;;
         for (int i = 1; i <= nums.length; i++) {
             int count = 0;
             for (int j = 0; j < nums.length; j++) {
@@ -11,6 +11,8 @@ public class Solution {
                 dup = i;
             else if (count == 0)
                 missing = i;
+            if (dup > 0 && missing > 0)
+                break;
         }
         return new int[] {dup, missing};
     }
