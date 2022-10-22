f605af3cd583049c08d8b4366965b632dea97058
Jay kim <90327424+jaykim-github@users.noreply.github.com>
2022-10-22 23:22:35 +0900

    Time: 3 ms (42.46%), Space: 43 MB (5.44%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean strongPasswordCheckerII(String password) {
+        Set<Character> seen = new HashSet<>();
+        for (int i = 0; i < password.length(); ++i) {
+            char c = password.charAt(i);
+            if (i > 0 && c == password.charAt(i - 1)) {
+                return false;
+            }
+            if (Character.isLowerCase(c)) {
+                seen.add('l');
+            }else if (Character.isUpperCase(c)) {
+                seen.add('u');
+            }else if (Character.isDigit(c)) {
+                seen.add('d');
+            }else {
+                seen.add('s');
+            }
+        }
+        return password.length() >= 8 && seen.size() == 4;
+    }
+}
